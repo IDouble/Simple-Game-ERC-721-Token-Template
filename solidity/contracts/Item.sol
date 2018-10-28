bea2a93e6af757b230bcfbe61940ca56f98b262a
AYIDouble <alpaypython@gmail.com>
2018-10-28 06:07:37 +0100

    🔁 Edit Solidity

---
+++ solidity/contracts/Item.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.24;
+
+contract Item is ERC721{
+    
+    struct Item{
+        string name; // Name of the Item
+        uint level; // Item Level
+        uint rarityLevel;  // 1 = normal, 2 = rare, 3 = epic, 4= legendary
+    }
+    
+    Item[] public items; // First Item has Index 0
+    address public owner;
+    
+    function Item() public {
+        owner = msg.sender; // The Sender is the Owner; Ethereum Address of the Owner
+    }
+    
+    function createItem(string _name, address _to) public{
+        require(owner == msg.sender); // Only the Owner can create Items
+        uint id = items.length; // Item ID = Length of the Array Items
+        items.push(Item(_name,5,1)) // Item ("Sword",5,1)
+        _mint(_to,id); // Assigns the Token to the Ethereum Address that is specified
+    }
+    
+}
